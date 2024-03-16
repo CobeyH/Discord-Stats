c7d04ea55434c496609db75c4d809075725516bd
Cobey Hollier <cobey.hollier@mac.com>
2024-03-16 16:33:07 +0000

    Adding search to top conversations

--- lib/top_conversations.dart
+++ lib/top_conversations.dart
@@ -4,19 +4,55 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'models/conversation.dart';
 
-class TopConversations extends ConsumerWidget {
+class TopConversations extends ConsumerStatefulWidget {
   const TopConversations({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
-    List<Conversation> conversations = ref.watch(conversationsProvider);
+  TopConversationsState createState() => TopConversationsState();
+}
+
+class TopConversationsState extends ConsumerState<TopConversations> {
+  String searchQuery = "";
+
+  List<Conversation> getFilteredConversations(
+      List<Conversation> conversations) {
+    if (searchQuery.isEmpty) {
+      return conversations;
+    }
     conversations.sort((a, b) => b.messages.length - a.messages.length);
-    return ListView.builder(
-      itemCount: conversations.length,
-      itemBuilder: (context, index) => ListTile(
-        title: Text("#$index ${conversations[index].name}"),
-        subtitle: Text(conversations[index].messages.length.toString()),
+    return conversations.where((conversation) {
+      return conversation.name
+          .toLowerCase()
+          .contains(searchQuery.toLowerCase());
+    }).toList();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    List<Conversation> conversations = ref.watch(conversationsProvider);
+    List<Conversation> filteredConversations =
+        getFilteredConversations(conversations);
+
+    return Column(children: [
+      TextField(
+        onChanged: (value) => setState(() => searchQuery = value),
+        decoration: InputDecoration(
+          labelText: "Search",
+          hintText: "Search",
+          prefixIcon: const Icon(Icons.search),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10),
+          ),
+        ),
       ),
-    );
+      ListView.builder(
+        itemCount: filteredConversations.length,
+        itemBuilder: (context, index) => ListTile(
+          title: Text("#$index ${filteredConversations[index].name}"),
+          subtitle:
+              Text(filteredConversations[index].messages.length.toString()),
+        ),
+      )
+    ]);
   }
 }
